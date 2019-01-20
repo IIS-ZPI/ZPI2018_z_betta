5aad8598fd8118f898932dbe74ec5ae0a4279d67
90rl <40734302+90rl@users.noreply.github.com>
2019-01-20 10:35:43 +0100

    Dodanie testów wydajnościowych do pobierania danych z url, zmiany przy metodach statystycznych

--- src/tests/pl/zpi/betta/DownloadDataTest.java
+++ src/tests/pl/zpi/betta/DownloadDataTest.java
@@ -2,13 +2,19 @@ package pl.zpi.betta;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 class DownloadDataTest {
+
+
+    private final long MAX_TIME_DOWNLOAD=2000; //2 sekundy w ms
+    boolean flagaFalseB = false;
+
 
 //test - pobieranie danych z dnia  ------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -46,7 +52,6 @@ class DownloadDataTest {
         assertEquals((float)x,map.get("2017-08-11"));
     }
 
-
     // test dla pobierania z api dla "JPY" - jen japoński
     @Test
     void testDownloadDataJpy() {
@@ -59,10 +64,21 @@ class DownloadDataTest {
         assertEquals((float)x,map.get("2019-01-03"));
     }
 
+//TEST WYDAJNOŚCIOWY  dla 1 dnia  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceDayDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-02/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
 
 //test - Sprawdzenie dla pobierania danych z tygodnia  -------------------------------------------------------------------------------------------------------------------------------------------
 @Test
-void weekDownloadTest(){
+void testWeekDownloadTest(){
    // System.out.println("Pobieranie danych dla tygodnia");
     DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-08/");
     Map<String, Float> map = download.getValue();
@@ -71,9 +87,23 @@ void weekDownloadTest(){
     assertEquals((float)x,map.size());
 }
 
-    //test - Sprawdzenie dla pobierania danych z dwóch tygodni  -------------------------------------------------------------------------------------------------------------------------------------------
+
+//TEST WYDAJNOŚCIOWY  dla 1 tygodnia  -------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void twoWeeksDownloadTest(){
+    void testPerformanceWeekDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-08/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        //System.out.println(flagaFalseB);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
+
+//test - Sprawdzenie dla pobierania danych z dwóch tygodni  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testTwoWeeksDownloadTest(){
      //   System.out.println("Pobieranie danych dla dwóch tygodni");
         DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-15/");
         Map<String, Float> map = download.getValue();
@@ -82,9 +112,21 @@ void weekDownloadTest(){
         assertEquals((float)x,map.size());
     }
 
+//TEST WYDAJNOŚCIOWY  dla 2 tygodni  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceTwoWeeksDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-15/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
+
 //test - Sprawdzenie dla pobierania danych z miesiąca  -------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void monthDownloadTest(){
+    void testMonthDownloadTest(){
      //   System.out.println("Pobieranie danych dla miesiaca");
         DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-31/");
         Map<String, Float> map = download.getValue();
@@ -92,10 +134,21 @@ void weekDownloadTest(){
         int x = 22; // zliczone na sztywno
         assertEquals((float)x,map.size());
     }
+//test WYDAJNOŚCIOWY  dla 1 miesiąca  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceMonthDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-01-31/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
 
     //test - Sprawdzenie dla pobierania danych z kwartału  -------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void quarterDownloadTest(){
+    void testQuarterDownloadTest(){
      //   System.out.println("Pobieranie danych dla kwartału");
         DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-03-31/");
         Map<String, Float> map = download.getValue();
@@ -104,9 +157,21 @@ void weekDownloadTest(){
         assertEquals((float)x,map.size());
     }
 
+//test WYDAJNOŚCIOWY  dla 1 kwartału  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceQuarterDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-03-31/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
+
     //test - Sprawdzenie dla pobierania danych z połowy roku  -------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void halfYearDownloadTest(){
+    void testHalfYearDownloadTest(){
         //System.out.println("Pobieranie danych dla połowy roku");
         DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-06-30/");
         Map<String, Float> map = download.getValue();
@@ -115,10 +180,21 @@ void weekDownloadTest(){
         assertEquals((float)x,map.size());
     }
 
+//test WYDAJNOŚCIOWY  dla połowy roku  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceHalfYearDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-06-30/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
 
     //test - Sprawdzenie dla pobierania danych z  roku  -------------------------------------------------------------------------------------------------------------------------------------------
     @Test
-    void yearDownloadTest(){
+    void testYearDownloadTest(){
      //   System.out.println("Pobieranie danych dla roku");
         DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-12-31/");
         Map<String, Float> map = download.getValue();
@@ -127,4 +203,15 @@ void weekDownloadTest(){
         assertEquals((float)x,map.size());
     }
 
+//test WYDAJNOŚCIOWY  dla roku  -------------------------------------------------------------------------------------------------------------------------------------------
+    @Test
+    void testPerformanceYearDownloadDataGbp() {
+        long startTimeDay = System.currentTimeMillis();
+        DownloadData download = new DownloadData("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2018-01-01/2018-12-31/");
+        long endTimeDay = System.currentTimeMillis();
+        //System.out.println(endTimeDay-startTimeDay);
+        //System.out.println(MAX_TIME_DOWNLOAD);
+        if(MAX_TIME_DOWNLOAD>endTimeDay-startTimeDay) org.junit.Assert.assertTrue(flagaFalseB = true);
+        assertEquals(true, flagaFalseB );
+    }
 }
